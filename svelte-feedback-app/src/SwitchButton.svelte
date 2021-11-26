98f23d23387ef9b134467047722dde9dd66cc3ba
eoghan <eoghan.firth@gmail.com>
2021-11-26 18:54:08 +0000

    init

---
+++ svelte-feedback-app/src/SwitchButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { Styles, Button } from 'sveltestrap';
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    function switchTokens() {
+        dispatch('switch',{});
+    }
+</script>
+
+<div>
+    <Button on:click={switchTokens}>⬇️</Button>
+</div>
+
+<Styles/>
---
+++ svelte-feedback-app/src/TokenChooser.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let description : string;
+    export let tokens = [];
+    export let selected;
+    export let amount = 0;
+</script>
+
+<div class="card">
+        <h5 class="card-title">
+            {description}
+        </h5>
+        <select bind:value={selected}>
+            {#each tokens as token}
+                <option value={token}>
+                    {token.symbol}
+                </option>
+            {/each}
+        </select>
+        <input type=number bind:value={amount} min=0>
+</div>
