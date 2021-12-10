f77ec7935b56be58029ff64592f6d9e6fdeb1af3
eoghan <eoghan.firth@gmail.com>
2021-12-10 19:38:39 +0000

    move QuoteParams into own file

--- svelte-feedback-app/src/models/UserQuery.ts
+++ svelte-feedback-app/src/models/UserQuery.ts
@@ -1,27 +1,22 @@
+import type { QuoteParams } from "./QuoteParams";
 import type { Token } from "./Token";
 
-export class UserQuery{
-    FromToken : Token | undefined;
-    ToToken : Token | undefined;
-    Amount : number | undefined;
+export class UserQuery {
+  FromToken: Token | undefined;
+  ToToken: Token | undefined;
+  Amount: number | undefined;
 
-    IsComplete() : boolean {
-        return !!this.FromToken && !!this.ToToken && !!this.Amount;
+  IsComplete(): boolean {
+    return !!this.FromToken && !!this.ToToken && !!this.Amount;
+  }
+
+  GetQuoteParams(): QuoteParams | undefined {
+    if (this.IsComplete()) {
+      return {
+        FromToken: this.FromToken!,
+        ToToken: this.ToToken!,
+        Amount: this.Amount!,
+      };
     }
-
-    GetQuoteParams() : QuoteParams | undefined {
-        if (this.IsComplete()){
-            return {
-                FromToken : this.FromToken!,
-                ToToken : this.ToToken!,
-                Amount : this.Amount!
-            }
-        }
-    }
-}
-
-export type QuoteParams = {
-    FromToken : Token,
-    ToToken : Token,
-    Amount : number
+  }
 }
